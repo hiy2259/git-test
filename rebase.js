80391ffa961ed40107df18ae6ef2912f9d95194d
김상우 <>
2023-12-21 16:05:37 +0900

    짝수의합(버그)

---
+++ rebase.js
@@ -0,0 +1,15 @@
+function solution() {
+
+  var sum = 0
+
+  for(var i = 1 ; i <= 10 ; i ++) {
+    if(i % 2 === 1) {
+      sum += i
+    }
+  }
+
+  console.log(sum)
+
+}
+
+var result = solution()
