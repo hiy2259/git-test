4abff98533e4fab05cbd77d935a1a98d670a12fa
김상우 <>
2023-12-21 16:07:01 +0900

    일부러 오타악

--- rebase.js
+++ rebase.js
@@ -2,8 +2,8 @@ function solution() {
 
   var sum = 0
 
-  for(var i = 1 ; i <= 20 ; i ++) {
-    if(i % 2 === 1) {
+  for(var i = 1 ; i <= 10 ; i ++) {
+    if(i % 2 === 0) {
       sum += i
     }
   }
